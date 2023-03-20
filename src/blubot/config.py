c2d7dbf3505ff2ad46c36cdd79258e6ca89a5bc1
blukat <blukatdevelopment@gmail.com>
2023-03-19 22:36:09 -0400

    Added config

---
+++ src/blubot/config.py
@@ -0,0 +1,11 @@
+# Config abstraction layer
+import json
+import sys
+
+def load_json(path):
+    with open(path, 'r') as file:
+        json_obj = file.read()
+        return json.loads(json_obj)
+
+def load_hexes():
+    return load_json('../../core_edition/keeper_info/campaigns/korsarus/hexes.json')
