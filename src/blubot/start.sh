fe8bffd0238d8812edae66db5db3b395990603e1
blukat <blukatdevelopment@gmail.com>
2023-03-23 23:20:10 -0400

    Made character editor some standalone html

--- src/blubot/start.sh
+++ src/blubot/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python3 bot.py &
+python3 bot.py #&
 
-export FLASK_APP=web/router.py
-flask run --host=0.0.0.0
+# Run a flask website
+#export FLASK_APP=web/router.py
+#flask run --host=0.0.0.0
