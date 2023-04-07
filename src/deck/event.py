41da37f01642307a60ef0f7ffba40343d6d5a1a8
blukat <blukatdevelopment@gmail.com>
2023-04-07 19:02:41 -0400

    Added click and drag and zooming in.

--- src/deck/event.py
+++ src/deck/event.py
@@ -9,7 +9,7 @@ KEYS = [
     pygame.K_SPACE,
     -1, # M_1
     -2, # M_2
-    -3  # M_3
+    -3, # M_3
 ]
 
 MOUSE_KEY_MAP = {
@@ -24,6 +24,8 @@ class EventState:
         self.M_1 = -1 # Left mouse click
         self.M_2 = -2 # middle mouse click
         self.M_3 = -3 # Right mouse click
+        self.MW_1 = -4 # mousewheel up
+        self.MW_2 = -5 # Mousewheel down
         self.DOWN = 0 # Just clicked
         self.HELD = 1 # preciously clicked, but not released
         self.UP = 2 # Just released
@@ -38,6 +40,10 @@ class EventState:
 
         self.mouse_x = 0
         self.mouse_y = 0
+        self.mouse_x_rel = 0
+        self.mouse_y_rel = 0
+        self.mousewheel_x = 0
+        self.mousewheel_y = 0
 
     def get_key(self, keycode):
         if keycode in self.keys:
@@ -55,8 +61,8 @@ class EventState:
     def key_unheld(self, keycode):
         return keycode in self.keys and self.keys[keycode] == self.UNHELD
 
-    # True if currently pressed, regardless of how long
-    def key_state(self, keycode):
+    # True if currently pressed, regardless of for how long
+    def key_pressed(self, keycode):
         return keycode in self.keys and self.keys[keycode] in [self.DOWN, self.HELD]
 
     # Shift current to previous
@@ -72,6 +78,9 @@ class EventState:
     def handle_events(self):
         self.prepare_key_states()
         (self.mouse_x, self.mouse_y) = pygame.mouse.get_pos()
+        (self.mouse_x_rel, self.mouse_y_rel) = pygame.mouse.get_rel()
+        self.mousewheel_x = 0
+        self.mousewheel_x = 0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.deck.stop_running()
@@ -79,7 +88,10 @@ class EventState:
                 self.keys[event.key] = self.DOWN
             elif event.type == pygame.KEYUP and event.key in KEYS:
                 self.keys[event.key] = self.UP
-            elif event.type == pygame.MOUSEBUTTONUP:
+            elif event.type == pygame.MOUSEBUTTONUP and event.button in MOUSE_KEY_MAP:
                 self.keys[MOUSE_KEY_MAP[event.button]] = self.UP
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button in MOUSE_KEY_MAP:
                 self.keys[MOUSE_KEY_MAP[event.button]] = self.DOWN
+            elif event.type == pygame.MOUSEWHEEL:
+                self.mousewheel_x = event.x
+                self.mousewheel_x = event.y
--- src/deck/hex/editor.py
+++ src/deck/hex/editor.py
@@ -1,5 +1,6 @@
 from hex.cell import Cell
 import pygame
+import random
 
 class HexEditor:
     def __init__(self, deck):
@@ -8,25 +9,58 @@ class HexEditor:
         self.screen = deck.get_screen()
         self.offset_x = 0
         self.offset_y = 0
+        self.ZOOM_MIN = 0.8
+        self.ZOOM_MAX = 4
         self.zoom = 1
+        self.scroll_speed = 10;
 
         self.cells = []
         self.populate_cells()
 
     def populate_cells(self):
-        self.cells.append(Cell((0, 255, 0), (5, 5)))
+        for x in range(50):
+            for y in range(50):
+                self.cells.append(Cell((0, random.randint(0, 255), 0), (x, y)))
 
     def update(self, event_state):
-        self.screen.fill("blue")
-        if event_state.key_down(event_state.M_1):
-            print("Click")
+        if event_state.key_down(event_state.M_3):
+            print("Right click")
+        if event_state.key_pressed(pygame.K_RIGHT):
+            self.offset_x -= self.scroll_speed
+        if event_state.key_pressed(pygame.K_LEFT):
+            self.offset_x += self.scroll_speed
+        if event_state.key_pressed(pygame.K_UP):
+            self.offset_y += self.scroll_speed
+        if event_state.key_pressed(pygame.K_DOWN):
+            self.offset_y -= self.scroll_speed
+        
+
+        space = event_state.key_pressed(pygame.K_SPACE)
+        m1 = event_state.key_pressed(event_state.M_1)
+        if space and m1:
+            # Dragging while holding space
+            self.offset_x += event_state.mouse_x_rel
+            self.offset_y += event_state.mouse_y_rel
+
+        if space:
+            self.scroll_speed = 30
+        else:
+            self.scroll_speed = 10
+
+
+        self.zoom += (event_state.mousewheel_x * 0.1)
+        self.zoom = self.ZOOM_MIN if self.zoom < self.ZOOM_MIN else self.zoom
+        self.zoom = self.ZOOM_MAX if self.zoom > self.ZOOM_MAX else self.zoom            
+        print(f"zoom: {self.zoom}")
         # Draw cells
         for cell in self.cells:
             self.draw_cell(cell)
 
     def draw_cell(self, cell):
         size = self.hex_size()
-        rect = pygame.Rect(cell.x * size, cell.y * size, size, size)
+        x = (cell.x * size) + self.offset_x
+        y = (cell.y * size) + self.offset_y
+        rect = pygame.Rect(x, y, size, size)
         color = pygame.Color(cell.r, cell.g, cell.b)
         self.pygame.draw.rect(self.screen, color, rect, 0)
 
